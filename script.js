63cdcf7a9016458f94eea6fac8c8d4b58e0f0da4
Joao Pedro Oliveira Barbosa <95698131+Iohanan-Cephas@users.noreply.github.com>
2026-07-29 10:37:43 -0300

    Adds computer's choice functions

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 console.log("hello world!");
+
+function cantBeZero() {
+    let x;
+    do {
+        x = Math.random();
+    } while(x === 0);
+
+    return x;
+}
+
+function getChoiceNumber(max) {
+    let x = cantBeZero();
+    return Math.floor(x * max);
+}
+
+function getComputerChoice() {
+    let x = getChoiceNumber(3);
+    let choice;
+
+    switch(x) {
+        case 0:
+            choice = "rock";
+            break;
+        case 1:
+            choice = "papel";
+            break;
+        case 2:
+            choice = "scissors";
+            break;
+    }
+
+    return choice;
+}
